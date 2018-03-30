6097b64ac1aacc729e69011e819007650b26de74
qq57694878 <57694878@qq.com>
2018-03-30 16:35:08 +0800

    init

---
+++ src/main/java/com/jldata/smartframe/SmartframeApplication.java
@@ -0,0 +1,12 @@
+package com.jldata.smartframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartframeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartframeApplication.class, args);
+	}
+}
